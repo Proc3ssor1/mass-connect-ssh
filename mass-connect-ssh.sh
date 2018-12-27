cff256dd3f26e6c990a74c4ab7eee4c7c8da0de2
Proc3ssor1 <35219258+Proc3ssor1@users.noreply.github.com>
2018-12-27 19:31:50 +0100

    Update mass-connect-ssh.sh

--- mass-connect-ssh.sh
+++ mass-connect-ssh.sh
@@ -5,9 +5,11 @@
 
 arr=()
 length=${#arr[@]}
+user="utilisateur_a_tester"
+mdp="mot_de_pass_a_tester"
 
 for ((i=0; i<$length; i++))
 do
-sshpass -p tea4tout ssh -o ConnectTimeout=2 -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no  root@${arr[i]} "hostname" >> host.txt
+sshpass -p $mdp ssh -o ConnectTimeout=1 -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no  $user@${arr[i]} "hostname" >> host.txt
 done
 
